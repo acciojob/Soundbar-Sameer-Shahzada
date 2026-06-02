58ab1fad2a77ea86ca1ec3b5211458483a63e4fe
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-06-02 16:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,30 +9,38 @@ const sounds = [
 
 const buttons = document.getElementById("buttons");
 
-sounds.forEach((sound) => {
+// Create audio elements
+sounds.forEach(sound => {
+  const audio = document.createElement("audio");
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+
   const btn = document.createElement("button");
   btn.className = "btn";
   btn.innerText = sound;
 
   btn.addEventListener("click", () => {
-    const audio = document.getElementById(sound);
-    audio.currentTime = 0;
+    stopSongs();
     audio.play();
   });
 
   buttons.appendChild(btn);
 });
 
+// Stop button
 const stopBtn = document.createElement("button");
 stopBtn.className = "stop";
 stopBtn.innerText = "stop";
 
-stopBtn.addEventListener("click", () => {
-  sounds.forEach((sound) => {
-    const audio = document.getElementById(sound);
-    audio.pause();
-    audio.currentTime = 0;
-  });
-});
+stopBtn.addEventListener("click", stopSongs);
 
 buttons.appendChild(stopBtn);
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
